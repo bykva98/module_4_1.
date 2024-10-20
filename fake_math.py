ca9e3e8da198a0b4639705b0186d4e83b8a9e4df
bykva98 <bykva.98@mail.ru>
2024-10-21 01:33:26 +0700

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,8 @@
+def divide (first, second):
+    if second > 0:
+        return first / second
+    elif second == 0:
+        return 'Ошибка'
+
+
+
---
+++ module_4_1.py
@@ -0,0 +1,12 @@
+import fake_math as fm_
+import true_math as tm_
+
+result1 = fm_.divide(69, 3)
+result2 = fm_.divide(3, 0)
+result3 = tm_.divide(49, 7)
+result4 = tm_.divide(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
+
